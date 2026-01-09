a1532793c70375c75bff7e45c05c46fadecee746
Ashish Verma <ashishkumar74979452@gmail.com>
2026-01-09 14:05:11 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-self.addEventListener("fetch", e => {});
+const CACHE_NAME = "nishchint-v1";
+const urlsToCache = ["/","/index.html","/logo.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
